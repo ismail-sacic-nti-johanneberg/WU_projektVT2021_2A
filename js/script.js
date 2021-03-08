3616b6e6d8ce6b6c512aa70492dd7531813ffee5
Ismail Sacic <70110297+ismail424@users.noreply.github.com>
2021-03-08 22:07:20 +0100

    Fixade index.html
    Har fixat hela index.html och nu ska jag börja med de adnra sidorna!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
+//Mobil knapp meny
 function mobileMeny(x) {
     x.classList.toggle("change");
     document.getElementById("nav").classList.toggle("nav_active");
+}
+
+
+// Scroll to top knapp
+var mybutton = document.getElementById("scroll_btn");
+
+//När usern scrollar ner så kommer knappen att komma upp (150px ner)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Functionen för att scrolla upp av sig själv
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
