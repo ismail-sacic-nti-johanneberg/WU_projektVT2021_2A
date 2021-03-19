5fae283225657d29dc1dcf0ba6014bd737c383d3
Ismail Sacic <>
2021-03-19 15:13:08 +0100

    Templates
    Skapade de andra sidorna som jag tänkte ha!

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 //Mobil knapp meny
-function mobileMeny(x) {
-    x.classList.toggle("change");
-    document.getElementById("nav").classList.toggle("nav_active");
+var mobile_meny = document.getElementById("mobile_meny")
+
+function mobileMeny() {
+  document.getElementById("mobile_meny").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("nav_active");
 }
 
 
@@ -9,7 +11,7 @@ function mobileMeny(x) {
 var mybutton = document.getElementById("scroll_btn");
 
 //När usern scrollar ner så kommer knappen att komma upp (150px ner)
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
@@ -24,3 +26,8 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+mybutton.addEventListener("click", topFunction)
+mobile_meny.addEventListener("click", mobileMeny)
+
